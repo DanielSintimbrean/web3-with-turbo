60250811864ab1ea46f1c702863115ab82f20c5b
DanielDS <ddsintim@inf.upv.es>
2023-03-08 20:21:24 +0100

    🎉 feat(wagmi): add wagmi provider

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
+import { WagmiProvider } from "~/providers/WagmiProvider";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,7 +12,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <WagmiProvider>
+        <Component {...pageProps} />
+      </WagmiProvider>
     </SessionProvider>
   );
 };
